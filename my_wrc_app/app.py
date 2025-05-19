cbe7cc483198279b78685d092cc66917fdbbd967
CodeMeisterGeneral <headradiomice@yahoo.co.uk>
2025-05-19 13:25:13 +0100

    typo fix

--- my_wrc_app/app.py
+++ my_wrc_app/app.py
@@ -2,9 +2,9 @@ import dash
 from dash import callback_context  
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-from wrc_app.auth import auth
-from wrc_app.layouts import base_layout
-from wrc_app.callbacks import register_callbacks
+from my_wrc_app.auth import auth
+from my_wrc_app.layouts import base_layout
+from my_wrc_app.callbacks import register_callbacks
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
